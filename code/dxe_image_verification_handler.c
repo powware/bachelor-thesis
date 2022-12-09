9ba2fbf7fcc1205f5eed692d55b7b51e7fd88c08
powware <powwared@gmail.com>
2022-12-09 16:20:15 +0100

    stuff

--- code/dxe_image_verification_handler.c
+++ code/dxe_image_verification_handler.c
@@ -1,22 +1,34 @@
-switch (GetImageType(File))
+#include <Uefi.h>
+#include <Protocol/DevicePath.h>
+
+EFI_STATUS
+EFIAPI
+DxeImageVerificationHandler(..., EFI_DEVICE_PATH_PROTOCOL *File, ...)
 {
-case IMAGE_FROM_FV:
-    Policy = ALWAYS_EXECUTE;
-    break;
+    // ...
 
-case IMAGE_FROM_OPTION_ROM:
-    Policy = PcdGet32(PcdOptionRomImageVerificationPolicy);
-    break;
+    switch (GetImageType(File))
+    {
+    case IMAGE_FROM_FV:
+        Policy = ALWAYS_EXECUTE;
+        break;
 
-case IMAGE_FROM_REMOVABLE_MEDIA:
-    Policy = PcdGet32(PcdRemovableMediaImageVerificationPolicy);
-    break;
+    case IMAGE_FROM_OPTION_ROM:
+        Policy = PcdGet32(PcdOptionRomImageVerificationPolicy);
+        break;
 
-case IMAGE_FROM_FIXED_MEDIA:
-    Policy = PcdGet32(PcdFixedMediaImageVerificationPolicy);
-    break;
+    case IMAGE_FROM_REMOVABLE_MEDIA:
+        Policy = PcdGet32(PcdRemovableMediaImageVerificationPolicy);
+        break;
 
-default:
-    Policy = DENY_EXECUTE_ON_SECURITY_VIOLATION;
-    break;
+    case IMAGE_FROM_FIXED_MEDIA:
+        Policy = PcdGet32(PcdFixedMediaImageVerificationPolicy);
+        break;
+
+    default:
+        Policy = DENY_EXECUTE_ON_SECURITY_VIOLATION;
+        break;
+    }
+
+    // ...
 }
